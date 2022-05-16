1f83ba5218b432642683c5a77a5f6c62b96f17a3
goreleaserbot <goreleaser@carlosbecker.com>
2022-05-16 20:17:46 +0300

    Brew formula update for syn-flood version v1.0.46

--- Formula/syn-flood.rb
+++ Formula/syn-flood.rb
@@ -5,12 +5,12 @@
 class SynFlood < Formula
   desc "syn-flood starts a syn flood attack with raw sockets. created for learning low level network ops"
   homepage "https://github.com/bilalcaliskan/syn-flood"
-  version "1.0.45"
+  version "1.0.46"
   license "apache-2.0"
 
   on_macos do
-    url "https://github.com/bilalcaliskan/syn-flood/releases/download/v1.0.45/syn-flood_1.0.45_darwin_x86_64.tar.gz"
-    sha256 "a212eba08465c3307ef7c8fd854e95beb9dde03d2446257044d39be9784e1856"
+    url "https://github.com/bilalcaliskan/syn-flood/releases/download/v1.0.46/syn-flood_1.0.46_darwin_x86_64.tar.gz"
+    sha256 "04500a03096243669028c16659237fa896502e221b9e63d98ee19646015cdef8"
 
     def install
       bin.install "syn-flood"
@@ -29,8 +29,8 @@ class SynFlood < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bilalcaliskan/syn-flood/releases/download/v1.0.45/syn-flood_1.0.45_linux_x86_64.tar.gz"
-      sha256 "1b168289b1a99c7c4138dc04b183f3585c231e37eb630374a84e0aa4fa9bdf0d"
+      url "https://github.com/bilalcaliskan/syn-flood/releases/download/v1.0.46/syn-flood_1.0.46_linux_x86_64.tar.gz"
+      sha256 "8e0cbba81ef95d83e9f27f04aaef9c7f757f2c61e9efff0b4c1ad989bc7416ee"
 
       def install
         bin.install "syn-flood"
